ef6222aa3ef9ce75cd8ae2aca8ed4be1375fd15c
Himanshu Mehta <hmehtaengg@gmail.com>
2021-12-14 22:58:26 +0530

    first commit

---
+++ src/api/weatherAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://api.openweathermap.org/data/2.5/",
+});
+
+const getWeatherBasedOnLocation = (location) => {
+  return apiClient
+    .get(
+      `/weather?q=${location}&APPID=${process.env.REACT_APP_WEATHER_APP_API}&units=metric`
+    )
+    .then((res) => res.data);
+};
+
+const getForecast = (lat, lon) => {
+  return apiClient
+    .get(
+      `/onecall?lat=${lat}&lon=${lon}&APPID=${process.env.REACT_APP_WEATHER_APP_API}&units=metric`
+    )
+    .then((res) => res.data);
+};
+
+export { getWeatherBasedOnLocation, getForecast };
+
+
+//89571534a582b5ec5969f03d148ea72e 
