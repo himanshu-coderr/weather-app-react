721054908189f55f75816395204ddd6fb8b5f28d
Himanshu Mehta <hmehtaengg@gmail.com>
2021-12-20 00:05:26 +0530

    Updated hourly forecast

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -4,6 +4,7 @@ import './forecast.scss'
 
 class Forecast extends React.Component {
   render() {
+    let flag = 0;
     const items = this.props.forecast.map((f, i) => {
       const image = {
         url: `http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`,
@@ -12,23 +13,32 @@ class Forecast extends React.Component {
       const description = f.weather[0].description;
       const unixTimestamp = f.dt;
       let hour = new Date(unixTimestamp * 1000).getHours();
-      let ampm = 'AM';
-      if (hour === 0) hour = 12; 
-      else if (hour > 12) {
-        hour = hour - 12;
-        ampm = 'PM';
+      console.log(hour)
+      
+
+      if(hour !== 0 && flag === 0){
+        let ampm = 'AM';
+        if (hour === 0) hour = 12; 
+        else if (hour > 12) {
+          hour = hour - 12;
+          ampm = 'PM';
+        }
+        return (
+          <div key={i} className="forecast-item">
+            <p className="forecast-item__time">{hour} {ampm}</p>
+            <p className="forecast-item__temp">
+              {f.temp} <span className="forecast-item__degree">°</span>
+            </p>
+            <img className="forecast-item__img" src={image.url} alt={image.alt} />
+            <p className="forecast-item__description">{description}</p>
+          </div>
+        );
+      }
+      else{
+        flag = 1
       }
 
-      return (
-        <div key={i} className="forecast-item">
-          <p className="forecast-item__time">{hour} {ampm}</p>
-          <p className="forecast-item__temp">
-            {f.temp} <span className="forecast-item__degree">°</span>
-          </p>
-          <img className="forecast-item__img" src={image.url} alt={image.alt} />
-          <p className="forecast-item__description">{description}</p>
-        </div>
-      );
+
     });
 
     let num = 1;
